6e6de4b4922e19b20d06d103bd0dba6e710a33dd
Szczepaniak Patrycja <szczepaniak7@gmail.com>
2018-05-05 21:03:50 +0200

    Utworzenie WireWorldApp.java, kontrolera do niego, pliku mainMeny.fxml oraz pliku css. Wstępne dodanie przycisków w menu.

---
+++ src/pl/edu/pw/ee/jimp2/gr11/wireworld/WireWorldAppController.java
@@ -0,0 +1,18 @@
+package pl.edu.pw.ee.jimp2.gr11.wireworld;
+
+import javafx.event.ActionEvent;
+
+public class WireWorldAppController {
+
+    public void pressNewGameButton(ActionEvent event){
+        System.out.println("tymczasowy napis");
+    }
+
+    public void pressRulesButton (ActionEvent event){
+        System.out.println("tymczasowy napis - zasady");
+    }
+
+    public void pressExitButton(ActionEvent event){
+
+    }
+}
